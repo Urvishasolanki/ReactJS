481bd005329aac7e6b8545cf0e37b20d7312b5ae
Urvishasolanki <solankiurvisha40@gmail.com>
2025-01-20 18:48:38 +0530

    dynamic-form

---
+++ dynamic-form/src/component/form.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import './form.css'
+
+function Di(){
+    const[formData,setformData] = useState({
+        name : "",
+        class : "",
+        email : "",
+        skills : [{skils : ""}]
+
+    })
+    const handleadd=()=>{
+        setformData({
+          ...formData,
+          skills :[...formData.skills,{skills : ""}]
+        })
+    }
+    const handlechange=(e)=>{
+        const {name,value} = e.target
+        if(isNaN(name)){
+       
+        setformData({...formData,[name]:value})
+        }
+        else{
+            const arr = [...formData.skills]
+            arr[name] = value
+            setformData({...formData,skills:arr})
+        }
+
+    }
+    console.log(formData)
+    
+    // const handlesubmit=()=>{
+    //     e.preventDefault()
+
+    // }
+
+    
+    return (
+    <div>
+        <div className="main"> 
+        <form action="" >
+            <input type="text" name="name" placeholder="Enter name"  onChange={handlechange}/>
+            <input type="text" name="class" placeholder="Enter class" onChange={handlechange}/>
+            <input type="text" name = "email" placeholder="Enter Email" onChange={handlechange}/>
+            {
+                formData.skills.map((el,index)=> (
+                    <input type="text" name={index} placeholder="Enter skils"  onChange={handlechange}/>
+                ))
+            }
+           
+        <input type="button"  value="add" onClick={handleadd}/> 
+        
+        </form>
+        </div>
+    </div>
+    )
+}
+export default Di
